5716eb85f9ed093e14c21a473977b08ae676dcb5
Gábor Egyed <gabor.egyed@gmail.com>
2024-07-04 08:55:43 +0200

    Fix date time range filter data extraction  (#24)

--- Filter/DataExtractor/Method/ValueKeysExtractionMethod.php
+++ Filter/DataExtractor/Method/ValueKeysExtractionMethod.php
@@ -53,9 +53,6 @@ class ValueKeysExtractionMethod implements DataExtractionMethodInterface
                         $values['value'][$key][$k] = $v;
                     }
                 }
-            } else {
-                dump($keys, $data);
-                throw new \InvalidArgumentException(sprintf('No value found for key "%s" in form data.', $key));
             }
         }
 
