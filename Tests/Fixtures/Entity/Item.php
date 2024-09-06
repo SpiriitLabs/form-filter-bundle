5eab54fb509253a12bd79c74a9173f52e940056f
Bart Heyrman <bartheyrman22@gmail.com>
2024-09-06 20:23:40 +0200

    Fix: must not be accessed before initialization errors

--- Tests/Fixtures/Entity/Item.php
+++ Tests/Fixtures/Entity/Item.php
@@ -26,10 +26,10 @@ class Item
     protected int $id;
 
     #[ORM\Column()]
-    protected string $name;
+    protected string $name = '';
 
     #[ORM\Column(type: 'integer')]
-    protected int $position;
+    protected ?int $position = null;
 
     #[ORM\Column(type: 'boolean')]
     protected bool $enabled;
@@ -41,7 +41,7 @@ class Item
     protected ?\DateTime $updatedAt = null;
 
     #[ORM\OneToMany(targetEntity: Options::class, mappedBy: 'item')]
-    private array $options;
+    private array $options = [];
 
     public function getId(): int
     {
@@ -58,12 +58,12 @@ class Item
         $this->name = $name;
     }
 
-    public function getPosition(): int
+    public function getPosition(): ?int
     {
         return $this->position;
     }
 
-    public function setPosition(int $position): void
+    public function setPosition(?int $position): void
     {
         $this->position = $position;
     }
